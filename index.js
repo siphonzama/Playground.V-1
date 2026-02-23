428891cc7d88d05249c88767857ff280477307ba
siphonzama <sipho1728@gmail.com>
2026-02-23 11:32:10 +0200

    second commit

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 import express from 'express';
 
 const app = express();
-let user = { name: "Unknown", role: "None" };
+let user = [];
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('<h1>Welcome to the playground!</h1>');
+})
+
+app.post('/Put', (req,res) => {
+    res.send('<h2>Put request received!</h2>');
+
+    const newUser = {
+    id: (user.length + 1).toString(),
+    name: req.body.name,
+    role: req.body.role
+    };
+
+    user.push(req.body);
+    console.log(req.body);
+    console.log(user);
+})
+
+app.get('/users', (req, res) => {
+    res.json(user)
 })
 
 app.post('/Post', (req, res) => {
@@ -16,8 +34,16 @@ app.post('/Post', (req, res) => {
 
 app.patch('/Patch', (req, res) => {
     res.send('<h2>patch request received!</h2>');
+    user = req.body;
     console.log(req.body);
 })
+
+app.get('/Patch', (req, res) => {
+    console.log('get request received!');
+    res.json(user);
+})
+
+console.log(user);
 
 app.listen(3000, () => {
     console.log('server is running on port 3000');
